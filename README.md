1147e39de941f717fc63a30a980b1eb1fef486f7
Eagle Edge <edgee8769@gmail.com>
2022-10-28 16:59:56 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# mainnet-rollup-economy
+
+In this repository, I analyze the dynaminc interplay between Ethereum mainnet and layer 2 rollups in the context of transaction fee and demand. Three research questions I want to explore are as follows:
+
+1. How does the transaction fee on Ethereum mainnet affect demands on mainnet and layer 2 rollups?
+2. How does the demand on the mainnet affect transaction fees on mainnet and layer 2 rollups?
+3. How does the transaction fee on layer 2 rollups affect the demands on mainnet and layer 2 rollups?
+
+I will start by collecting data on block-level and transaction-level using web3 API.
---
+++ web3_api.py
@@ -0,0 +1,30 @@
+from web3 import Web3
+from hexbytes import HexBytes
+
+# Connect to Infura endpoint using web3 API
+provider_url = 'https://mainnet.infura.io/v3/<YOUR INFURA ACCOUNT API KEY>'
+w3 = Web3(Web3.HTTPProvider(provider_url))
+w3.isConnected()
+
+# Function for retrieving transaction receipt
+def get_tx_receipt(txhash):
+    assert(type(txhash)==HexBytes)
+    receipt = w3.eth.getTransactionReceipt(txhash)
+    return receipt
+
+# Function for retrieving block information
+# detail=True gives detailed information about each transaction included in the block
+# detail=False lists transactions included in the block wihtout transaction details
+def get_block_info(blockno, detail=True):
+    info = w3.eth.getBlock(blockno, detail)
+    return info
+
+# Retrieve transaction information
+def get_tx_info(txhash):
+    assert(type(txhash)==HexBytes)
+    info = w3.eth.getTransaction(txhash)
+    return info
+
+# This function converts a random string to a HexBytes.
+def sha3(s:str):
+    return Web3.sha3(text=s)
